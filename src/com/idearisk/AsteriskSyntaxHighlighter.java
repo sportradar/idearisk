4acd0310a0aa7f32840f25d48132180b5fde0404
Even <e.lislebo@sportradar.com>
2016-10-14 08:38:09 +0200

    FEATURE: Added some syntax highlighting, quite rubbish atm.

--- src/com/idearisk/AsteriskSyntaxHighlighter.java
+++ src/com/idearisk/AsteriskSyntaxHighlighter.java
@@ -1,6 +1,7 @@
 package com.idearisk;
 
 import com.idearisk.psi.AsteriskTypes;
+import com.intellij.ide.highlighter.JavaHighlightingColors;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
 import com.intellij.openapi.editor.HighlighterColors;
@@ -24,10 +25,23 @@ public class AsteriskSyntaxHighlighter extends SyntaxHighlighterBase {
       createTextAttributesKey("ASTERISK_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
   public static final TextAttributesKey BAD_CHARACTER =
       createTextAttributesKey("ASTERISK_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+	public static final TextAttributesKey VARIABLE =
+			createTextAttributesKey("ASTERISK_VARIABLE", DefaultLanguageHighlighterColors.INSTANCE_FIELD);
+	public static final TextAttributesKey APPLICATION_CALL =
+			createTextAttributesKey("ASTERISK_APPLICATION_CALL", DefaultLanguageHighlighterColors.STATIC_FIELD);
+	public static final TextAttributesKey APPLICATION_ARGS =
+			createTextAttributesKey("ASTERISK_APPLICATION_ARGS", DefaultLanguageHighlighterColors.STATIC_METHOD);
+	public static final TextAttributesKey INCLUDE =
+			createTextAttributesKey("ASTERISK_INCLUDE", JavaHighlightingColors.ANNOTATION_NAME_ATTRIBUTES);
 
   private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
   private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
+	private static final TextAttributesKey[] KEYWORDS = new TextAttributesKey[]{KEY};
   private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+	private static final TextAttributesKey[] VARIABLES = new TextAttributesKey[]{VARIABLE};
+	private static final TextAttributesKey[] APPLICATION_CALLS = new TextAttributesKey[]{APPLICATION_CALL};
+	private static final TextAttributesKey[] APPLICATION_ARGUMENTS = new TextAttributesKey[]{APPLICATION_ARGS};
+	private static final TextAttributesKey[] INCLUDES = new TextAttributesKey[]{INCLUDE};
 
 	@NotNull @Override public Lexer getHighlightingLexer() {
 		return new AsteriskLexerAdapter();
@@ -38,8 +52,33 @@ public class AsteriskSyntaxHighlighter extends SyntaxHighlighterBase {
       return COMMENT_KEYS;
     } else if (tokenType.equals(TokenType.BAD_CHARACTER)) {
       return BAD_CHAR_KEYS;
+		} else if (isKeyword(tokenType)) {
+			return KEYWORDS;
+		} else if (tokenType.equals(AsteriskTypes.VAR_NAME)) {
+			return VARIABLES;
+		} else if (tokenType.equals(AsteriskTypes.APPLICATION_NAME)) {
+			return APPLICATION_CALLS;
+		} else if (tokenType.equals(AsteriskTypes.APPLICATION_ARGS)) {
+			return APPLICATION_ARGUMENTS;
+		} else if (isInclude(tokenType)) {
+			return INCLUDES;
 		} else {
       return EMPTY_KEYS;
     }
+	}
+
+	private static boolean isInclude(IElementType tokenType) {
+		return tokenType.equals(AsteriskTypes.INCLUDE_STMT)
+				|| tokenType.equals(AsteriskTypes.INCLUDE_FILE_TARGET);
+	}
+
+	private static boolean isApplicationCall(IElementType tokenType) {
+		return tokenType.equals(AsteriskTypes.APPLICATION_NAME)
+				|| tokenType.equals(AsteriskTypes.APPLICATION_ARGS);
+	}
+
+	private static boolean isKeyword(IElementType tokenType) {
+		return tokenType.equals(AsteriskTypes.EXT_INST_LEFT)
+				|| tokenType.equals(AsteriskTypes.INCLUDE_CTX_LEFT);
 	}
 }
